3d545fcafa570d5f1958994bfe63d2fdd773e58c
JIGYASU SINGH <jigyashu2001@gmail.com>
2023-12-06 13:59:45 +0530

    first commit

---
+++ Main.py
@@ -0,0 +1,174 @@
+import cv2
+import numpy as np
+
+def stackImages(imgArray, scale, labels=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        hor_con = np.concatenate(imgArray)
+        ver = hor
+    if len(labels) != 0:
+        eachImgWidth = int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        for d in range(0, rows):
+            for c in range(0, cols):
+                cv2.rectangle(ver, (c * eachImgWidth, eachImgHeight * d),
+                              (c * eachImgWidth + len(labels[d]) * 13 + 27, 30 + eachImgHeight * d),
+                              (255, 255, 255), cv2.FILLED)
+                cv2.putText(ver, str(labels[d]), (eachImgWidth * c + 10, eachImgHeight * d + 20),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 255), 2)
+    return ver
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4, 2))
+    myPointsNew = np.zeros((4, 1, 2), dtype=np.int32)
+    add = myPoints.sum(1)
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis=1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+    return myPointsNew
+
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 5000:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    return biggest, max_area
+
+def drawRectangle(img, biggest, thickness):
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 0), thickness)
+    return img
+
+def nothing(x):
+    pass
+
+def initializeTrackbars(initialTrackbarVals=(200, 200)):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Threshold1", "Trackbars", initialTrackbarVals[0], 255, nothing)
+    cv2.createTrackbar("Threshold2", "Trackbars", initialTrackbarVals[1], 255, nothing)
+
+def valTrackbars():
+    Threshold1 = cv2.getTrackbarPos("Threshold1", "Trackbars")
+    Threshold2 = cv2.getTrackbarPos("Threshold2", "Trackbars")
+    src = Threshold1, Threshold2
+    return src
+
+webCamFeed = True
+pathImage = "1.jpg"
+cap = cv2.VideoCapture(0)
+cap.set(10, 160)
+heightImg = 640
+widthImg = 480
+
+initializeTrackbars()
+count = 0
+
+while True:
+    if webCamFeed:
+        success, img = cap.read()
+        if not success:
+            print("Error: Failed to read from webcam.")
+            break
+    else:
+        img = cv2.imread(pathImage)
+        if img is None:
+            print(f"Error: Could not read the image from {pathImage}.")
+            break
+
+    img = cv2.resize(img, (widthImg, heightImg))
+    imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+
+    thres = valTrackbars()
+    imgThreshold = cv2.Canny(imgBlur, thres[0], thres[1])
+
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)
+
+    imgContours = img.copy()
+    imgBigContour = img.copy()
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
+
+    biggest, maxArea = biggestContour(contours)
+
+    if biggest.size != 0:
+        biggest = reorder(biggest)
+        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)
+        imgBigContour = drawRectangle(imgBigContour, biggest, 2)
+        pts1 = np.float32(biggest)
+        pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+        imgWarpColored = imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+        imgWarpColored = cv2.resize(imgWarpColored, (widthImg, heightImg))
+
+        imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+        imgAdaptiveThre = cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+        imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+        imgAdaptiveThre = cv2.medianBlur(imgAdaptiveThre, 3)
+
+        imageArray = ([img, imgGray, imgThreshold, imgContours],
+                      [imgBigContour, imgWarpColored, imgWarpGray, imgAdaptiveThre])
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            cv2.imwrite("E:/project/myImage" + str(count) + ".jpg", imgWarpColored)
+            cv2.rectangle(imageArray[1][3], ((int(imageArray[1][3].shape[1] / 2) - 230),
+                                              int(imageArray[1][3].shape[0] / 2) + 50),
+                          (1100, 350), (0, 255, 0), cv2.FILLED)
+            cv2.putText(imageArray[1][3], "Scan Saved",
+                        (int(imageArray[1][3].shape[1] / 2) - 200, int(imageArray[1][3].shape[0] / 2)),
+                        cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+            cv2.imshow('Result', imageArray[1][3])
+            cv2.waitKey(300)
+            count += 1
+    else:
+        imageArray = ([img, imgGray, imgThreshold, imgContours],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
+
+    labels = [["Original", "Gray", "Threshold", "Contours"],
+              ["Biggest Contour", "Warp Perspective", "Warp Gray", "Adaptive Threshold"]]
+
+    stackedImage = stackImages(imageArray, 0.75, labels)
+    cv2.imshow("Result", stackedImage)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC key
+        cap.release()
+        cv2.destroyAllWindows()
+        break
